ff3d7312800482ee8eda4d23d70bb4098004920e
Ethan Kennerly <ethan@lumoslabs.com>
2017-04-22 12:08:37 -0700

    Ignored eating on different layer after 2 attempts

--- Assets/Scripts/PhotonBody.cs
+++ Assets/Scripts/PhotonBody.cs
@@ -6,7 +6,6 @@ namespace FineGameDesign.SmallWorld
 	public sealed class PhotonBody : MonoBehaviour
 	{
 		public static float eatRadiusThreshold = 1.125f;
-		public static float ignoreEatLayer = 2;
 
 	 	PhotonView photon;
 	 	Rigidbody2D body;
@@ -60,16 +59,16 @@ namespace FineGameDesign.SmallWorld
 		void OnCollisionEnter2D(Collision2D other)
 		{
 			GameObject otherObject = other.gameObject;
-			Debug.Log("OnCollisionEnter2D: layer " + otherObject.layer
-				+ " ignore " + ignoreEatLayer);
-			if (ignoreEatLayer == otherObject.layer)
+			Debug.Log("OnCollisionEnter2D: other layer " + otherObject.layer
+				+ " my layer " + gameObject.layer);
+			if (gameObject.layer != otherObject.layer)
 			{
 				return;
 			}
-			MayEat(otherObject);
+			MayEatSmaller(otherObject);
 		}
 
-		void MayEat(GameObject otherObject)
+		void MayEatSmaller(GameObject otherObject)
 		{
 			Vector3 otherScale = otherObject.transform.localScale;
 			float otherRadius = Mathf.Abs(otherScale.x);

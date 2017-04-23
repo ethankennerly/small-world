2ff329e73d3df7459bb95f4c3f22e0b1d4b3349a
Ethan Kennerly <ethan@lumoslabs.com>
2017-04-23 09:21:22 -0700

    Stubbed cell bot script.

---
+++ Assets/Scripts/CellBot.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Finegamedesign.SmallWorld
+{
+	public sealed class CellBot : MonoBehaviour
+	{
+		public float eatRelativeScale = 0.875f;
+		public float fleeRelativeScale = 1.25f;
+	}
+}
